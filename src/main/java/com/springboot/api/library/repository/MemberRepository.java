bb4b61ab841a761b23889599e02a20e1e730ce15
Juanjo <juanjose.ortizgarcia@outlook.com>
2023-07-29 20:35:57 +0200

    Add MemberRepository

--- src/main/java/com/springboot/api/library/repository/MemberRepository.java
+++ src/main/java/com/springboot/api/library/repository/MemberRepository.java
@@ -1,2 +1,7 @@
-package com.springboot.api.library.repository;public interface MemberRepository {
+package com.springboot.api.library.repository;
+
+import com.springboot.api.library.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
