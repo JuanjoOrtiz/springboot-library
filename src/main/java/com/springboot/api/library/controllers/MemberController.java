98c254feb888aef9f4ef849e6c31c714dba55f4a
Juanjo <juanjose.ortizgarcia@outlook.com>
2023-09-14 18:03:47 +0200

    findAll() corrected

--- src/main/java/com/springboot/api/library/controllers/MemberController.java
+++ src/main/java/com/springboot/api/library/controllers/MemberController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class MemberController {
 
     @Autowired
